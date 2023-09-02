c649f55860f1aab10f74eb4c2a3f92fc167c848a
JonathanBaquero01 <78324860+JonathanBaquero01@users.noreply.github.com>
2023-09-02 18:32:28 -0500

    Add files via upload

--- Lain-BD.sql
+++ Lain-BD.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: lain
 -- ------------------------------------------------------
--- Server version	8.0.19
+-- Server version	8.0.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `claveadmin`
 --
-
+CREATE DATABASE IF NOT EXISTS `lAIN` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `lain`;
 DROP TABLE IF EXISTS `claveadmin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -125,4 +126,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-23 17:15:22
+-- Dump completed on 2023-09-02 18:26:26
